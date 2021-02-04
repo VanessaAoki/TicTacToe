cbf3d9f2c591357ead73ceab5ca020864ce67635
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-04 13:16:19 -0300

    Added a mockup game

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,51 @@
-p "Welcome to tic-tac-toe. Do you know how to play it? (yes/no)"
+puts "Welcome to tic-tac-toe. Do you know how to play it? (yes/no)"
+HowTo = gets.chomp.downcase
 
-if "yes"
-  p "Ok, let's start the game."
+if HowTo == "yes"
+  puts "Ok, let's start the game!"
 else
-  p "No problem, below are the tips to understand how to play the game:"
-  p "In order to win the game, a player must place three of their marks in a horizontal, vertical, or diagonal row."
-  p ""
+  puts "No problem, below are the tips to understand how to play the game:"
+  puts "In order to win the game, a player must place three of their marks in a horizontal, vertical, or diagonal row."
+  puts "Ok, let's start the game!"
 end
 
 # Naming players
-p "First of all, what's your name? "
-
-p "Ok, player 2, what's your name? "
-
-
-# Randomizing players
-p "Now, #{player1} do you choose heads or tails? "
-
+puts "First of all, insert the name of player 'X': "
+playerX = gets.chomp
+puts "Ok, player 'O', what's your name? "
+playerO = gets.chomp
 
 # First move
-p "#{player}, you must choose your move: "
+puts "#{playerX}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1     |     |
+-------------------  
+2     |     |
+-------------------  
+3     |     |
+"
 
+move1 = gets.chomp
+
+# Second move
+puts "#{playerO}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |     |
+-------------------  
+2     |     |
+-------------------  
+3     |     |
+"
+
+move2 = gets.chomp
+
+# Third move
+puts "#{playerX}, you must choose your move: "
+puts "
    a  |  b  |  c
 -------------------
 1  X  |  O  |
@@ -28,68 +53,107 @@ p "#{player}, you must choose your move: "
 2     |     |
 -------------------  
 3     |     |
+"
 
-# Second move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
-
-# Third move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+move3 = gets.chomp
 
 # Forth move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerO}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2     |  X  |
+-------------------  
+3     |     |
+"
+
+move4 = gets.chomp
 
 # Fifth move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerX}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2     |  X  |
+-------------------  
+3     |     |  O
+"
+
+move5 = gets.chomp
 
 # Sixth move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerO}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2     |  X  |
+-------------------  
+3     |  X  |  O
+"
+
+move6 = gets.chomp
 
 # Seventh move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerX}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2     |  X  |
+-------------------  
+3  0  |  X  |  O
+"
+
+move7 = gets.chomp
 
 # Eigth move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerO}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2  X  |  X  |
+-------------------  
+3  0  |  X  |  O
+"
+
+move8 = gets.chomp
 
 # Nineth move
-if a1, a2, a3, b1, b2, b3, c1, c2, c3
-  p "Great, #{player} now it's your turn, choose your move: "
-else
-  p "Oh, I'm sorry, this is not a valid move."
-end
+puts "#{playerX}, you must choose your move: "
+puts "
+   a  |  b  |  c
+-------------------
+1  X  |  O  |
+-------------------  
+2  X  |  X  |  O
+-------------------  
+3  0  |  X  |  O
+"
+
+move9 = gets.chomp
+
+# Tenth move
+puts " GAME OVER!
+
+   a  |  b  |  c
+-------------------
+1  X  |  O  |  X
+-------------------  
+2  X  |  X  |  O
+-------------------  
+3  0  |  X  |  O
+
+"
 
 # Game over
-if player1 wins
-  p "Congratulations #{player1} you WON!"
-elsif player2 wins
-  p "Congratulations #{player2} you WON!"
-else
-  p "It's a DRAW, restart the game?"
-end
+puts "It's a DRAW, restart the game? (yes/no)"
+restart = gets.chomp
