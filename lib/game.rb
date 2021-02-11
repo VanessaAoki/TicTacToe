055214f200cc2d7486717a63c6a1e7ca210d9aca
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-11 13:32:06 -0300

    Update the game file

--- lib/game.rb
+++ lib/game.rb
@@ -10,9 +10,9 @@ class TicTacToe
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     puts "----------"
   end
 
