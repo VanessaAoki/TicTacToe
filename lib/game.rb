ac6158184469fddc9a67a0c8990c369f97a29c49
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-02-11 14:10:50 -0300

    Fix the linters

--- lib/game.rb
+++ lib/game.rb
@@ -1,31 +1,28 @@
-# frozen_string_literal: true
-
-require_relative './players'
-
+# Game logic
 class TicTacToe
-  def initialize(board=nil)
-    @board = Array.new(9, " ")
+  def initialize(_board = nil)
+    @board = Array.new(9, ' ')
   end
-  
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "----------"
+    puts '----------'
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "----------"
+    puts '----------'
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    puts "----------"
+    puts '----------'
   end
 
   WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
 
   def input_to_index(user_input)
     user_input.to_i - 1
